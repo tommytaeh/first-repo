4f0fed792833f0b2dd702197c6f8c76508e2832a
tommytaeh <tommytae9697@gmail.com>
2021-08-20 17:17:11 +0900

    feat: Change inner variable, range, string
    inner variable: i to j
    range: 10 to 20
    string
    - '{j} is even.' if j is even
    - j if j is odd.

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-for i in range(1, 10+1):
+for j in range(1, 20+1):
     # print 'hello' if i is even.
-    if i%2==0:
-        print('hello')
+    if j%2==0:
+        print(f'{j} is even.')
+    else:
+        print(j)
